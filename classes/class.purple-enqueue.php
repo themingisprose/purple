73fbc0a8ba8f8ddd0103243e586c5826e33426ae
rogertm <roger213tm@gmail.com>
2021-05-02 13:30:45 -0400

    Class Purple_Enqueue

---
+++ classes/class.purple-enqueue.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Enqueue styles and scripts
+ *
+ * @since Purple 1.0.0
+ */
+class Purple_Enqueue
+{
+
+	/**
+	 * @var null
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Enqueue
+	 *
+	 * @since Purple 1.0.0
+	 */
+	public function init()
+	{
+		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
+	}
+
+	/**
+	 * Handle styles
+	 *
+	 * @since Purple 1.0.0
+	 */
+	public function styles()
+	{
+		wp_register_style( 'purple-style', get_stylesheet_directory_uri() .'/assets/dist/css/purple-style.css', array(), $this->theme_version() );
+		wp_enqueue_style( 'purple-style' );
+	}
+
+	/**
+	 * Get the theme version
+	 *
+	 * @since Purple 1.0.0
+	 */
+	public function theme_version()
+	{
+		return wp_get_theme()->get( 'Version' );
+	}
+
+	/**
+	 * Get Instance
+	 *
+	 * @since Purple 1.0.0
+	 */
+    public static function get_instance() : self
+    {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+Purple_Enqueue::get_instance()->init();
