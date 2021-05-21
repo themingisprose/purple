799bb0e46392b3db9702e42236c2b4340a0ed233
rogertm <roger213tm@gmail.com>
2021-05-21 09:10:14 -0400

    Class Purple_Admin_Google_Tracking Renombrada la clase Purple_Admin_Google_Tag_Manager por Purple_Admin_Google_Tracking y añadido un campo para Google Analytics

--- templates/general.php
+++ templates/general.php
@@ -123,3 +123,13 @@ function purple_google_tag_manager_body(){
 	theming_option( 'tag_manager_body' );
 }
 add_action( 'wp_body_open', 'purple_google_tag_manager_body' );
+
+/**
+ * Google Analytics code
+ *
+ * @since Purple 1.0.0
+ */
+function purple_google_analytics(){
+	theming_option( 'analytics' );
+}
+add_action( 'wp_head', 'purple_google_analytics' );
